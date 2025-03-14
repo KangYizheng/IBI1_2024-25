ddae274548aa797d70227bfced656289a9aa6165
Yizheng <3275014244@qq.com>
2025-03-14 11:50:12 +0800

    7

--- Practical4/bmi_calculator.py
+++ Practical4/bmi_calculator.py
@@ -1,7 +1,13 @@
+#input the weight and height of a person and calculates the BMI of the person
+#calculate the BMI of a person
+#print out the BMI of the person
+#determine if the person is underweight, normal weight or obese
+
+
 Weight=float(input("the weight of person the weight is in kg")) 
-Height=float(input("the height of person  in m"))
-BMI= Weight/(Height**2) #Use the formula to calculate BMI
-if BMI > 30: #BMI is a measure of body fat based on height and weight asd
+Height=float(input("the height of person in m"))
+BMI= Weight/(Height**2) #Use the formula to calculate BMI, BMI is a measure of body fat based on height and weight asd
+if BMI > 30: 
    print("your BMI:",str(BMI),"You are obese")
 elif BMI < 18.5:
    print("your BMI:",str(BMI),"You are underweight")
