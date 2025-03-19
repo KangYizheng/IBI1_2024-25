5c1154a263d83d605a1502d983cfec4257354c26
Yizheng <3275014244@qq.com>
2025-03-19 20:37:45 +0800

    2

--- Practical5/experiment2.py
+++ Practical5/experiment2.py
@@ -1,5 +1,5 @@
 UK_country_population = [57.11, 3.13, 1.91, 5.45]
-China_country_populations = [65.77,41.88,45.28, 61.27,85.15]
+China_country_populations = [41.88,45.28, 61.27,85.15]
 sorted_UK = sorted(UK_country_population)
 sorted_China = sorted(China_country_populations)
 print(sorted_UK)
@@ -15,10 +15,10 @@ plt.axis('equal')
 plt.show()
 
 import matplotlib.pyplot as plt
-labels = "Guangdong", "Shandong", "Henan", "Sichuan", "Jiangsu"
-sizes = [65.77,41.88,45.28, 61.27,85.15]
-colors = ['gold', 'yellowgreen', 'lightcoral', 'lightskyblue', 'red']
-explode = (0.1, 0, 0, 0, 0)
+labels ="Fujian","Jiangxi","Anhui","Jiangsu"
+sizes = [41.88,45.28, 61.27,85.15]
+colors = ['gold', 'yellowgreen', 'lightcoral', 'lightskyblue']
+explode = (0.1, 0, 0, 0)
 plt.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%', shadow=False, startangle=90)
 plt.axis('equal')
 plt.show()
