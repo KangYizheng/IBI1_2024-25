9b1b11e71ab870e8eeb0df37eb27a3af037d6f3a
Yizheng <3275014244@qq.com>
2025-03-27 22:38:35 +0800

    1

--- Practical5/experiment1.py
+++ Practical5/experiment1.py
@@ -1,8 +1,13 @@
+#plan
+#forming a dictionary with programming languages and their popularity percentages
+#plot a bar graph to show the popularity of the top 5 programming languages in the dictionary
+#check if the language "JavaScript" is present in the dictionary. If it is present, print the percentage of users who use JavaScript.
+
 language_data={"JavaScript":62.3,"HTML":52.9,"Python":51,"SQL":51,"TypeScript":38.5} #dictionary containing the data
 # 1. Plot a bar graph to show the popularity of the top 5 programming languages in the dictionary.
 print(language_data)
 import matplotlib.pyplot as plt
-
+# Extracting the keys and values from the dictionary
 languages = list(language_data.keys()) 
 percentages = list(language_data.values())
 
