73682be32fa604c38bfb8f84a0c9365690609af1
Yizheng <3275014244@qq.com>
2025-04-03 15:30:57 +0800

    edit precode

--- Practical7/spliced_tata_genes.py
+++ Practical7/spliced_tata_genes.py
@@ -1,24 +1,28 @@
 import re
-input_donor=input("Enter the type of recepter(GTAG,GCAG,ATAC) ")# input types of gene
-input_receptor=input("Enter the type of recepter(GTAG,GCAG,ATAC) ")# input types of gene   
+input_donor=input("Enter the type of recepter(GTAG,GCAG,ATAC) ")# input types of donor
+input_receptor=input("Enter the type of recepter(GTAG,GCAG,ATAC) ")# input types of receptor
+# input the type of donor and receptor   
 output_file=f"{input_donor}{input_receptor}_spliced_genes.fa"   
-if f"{input_donor}{input_receptor}" not in ['GTAG','GCAG','ATAC']:
+if f"{input_donor}{input_receptor}" not in ['GTAG','GCAG','ATAC']:# check if the input is valid
     print("Invalid input. Please enter GTAG, GCAG, or ATAC.")
-with open ("C:/Users/ASUS/Desktop/第二学期/Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa",'r') as infile, open (output_file,'w') as outfile:
+with open ("C:/Users/ASUS/Desktop/第二学期/Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa",'r') as infile, open (output_file,'w') as outfile:# open the input and output files
     current_gene_name = ""
     current_sequence = ""
-    for line in infile:
+    for line in infile:       # read the input file line by line
+        # check if the line is a header line                            
         if re.search('^>', line):
-            if  re.search(fr'TATA[AT]A[AT]',current_sequence) and re.search(fr'{input_donor}.*{input_receptor}',current_sequence):               
-                    time=len(re.findall(r'TATA[AT]A[AT]',current_sequence))      
-                    outfile.write(f">{current_gene_name} time={time}\n{current_sequence}\n")
+            if  re.search(fr'TATA[AT]A[AT]',current_sequence) and re.search(fr'{input_donor}.*{input_receptor}',current_sequence):              
+                    time=len(re.findall(r'TATA[AT]A[AT]',current_sequence))     #count the number of TATA boxes in the sequence 
+                    outfile.write(f">{current_gene_name} time={time}\n{current_sequence}\n") # write the gene name, time, and sequence to the output file
+            # Extract the gene name from the header line
             current_gene_name=re.search(r"gene:.*?\s",line) 
-            if current_gene_name:
+            if current_gene_name:# delete the gene: from the header line
                     current_gene_name = current_gene_name[0]
                     current_gene_name = re.sub(r'gene:','',current_gene_name)
             current_sequence = ""
         else:
-            current_sequence += line
+            current_sequence += line # append the sequence to the current sequence
+            # Remove new line characters from the sequence
             current_sequence = re.sub(r'\n','',current_sequence)
         # check for the last gene in the file
     if re.search(fr'{input_donor}.*TATA[AT]A[AT].*{input_receptor}',current_sequence):
--- Practical7/tata_box.py
+++ Practical7/tata_box.py
@@ -3,19 +3,20 @@ output_file=f"tata_genes.fa" # output file name
 with open ("C:/Users/ASUS/Desktop/第二学期/Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa",'r') as infile, open (output_file,'w') as outfile:# open the input and output files
     current_gene_name = ""
     current_sequence = ""
-    for line in infile:
-        if re.search('^>', line):
+    for line in infile:# read the input file line by line
+        if re.search('^>', line):# check if the line is a header line
+            # check if the previous sequence contains a TATA box
             if  re.search(r'TATA[AT]A[AT]',current_sequence):
                 outfile.write(f">{current_gene_name}\n{current_sequence}\n")
             current_gene_name=re.search(r"gene:.*?\s",line) 
-            if current_gene_name:
+            if current_gene_name:# delete the gene: from the header line
                     current_gene_name = current_gene_name[0]
                     current_gene_name = re.sub(r'gene:','',current_gene_name)
             current_sequence = ""
             
         else:
-            current_sequence += line
-            current_sequence = re.sub(r'\n','',current_sequence)
+            current_sequence += line # append the sequence to the current sequence
+            current_sequence = re.sub(r'\n','',current_sequence)## remove new line characters from the sequence
         # check for the last gene in the file
     if re.search('TATA[AT]A[AT]',current_sequence):
        outfile.write(f"{current_gene_name}\n{current_sequence}\n")
