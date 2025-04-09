b41a41183d05d1df8884e1fa300b6fae49bd2865
Yizheng <3275014244@qq.com>
2025-04-09 15:14:56 +0800

    drug_dosage

--- Practical8/Restriction_enzyme_cut_sites.py
+++ Practical8/Restriction_enzyme_cut_sites.py
@@ -1,14 +1,14 @@
 import re
 def find_the_restriction_enzyme_cut_sites(sequence, enzyme):# function to find the restriction enzyme cut sites in a DNA sequence
-    if re.search(r'ACGT', sequence):# check if the sequence is a valid DNA sequence
-        if re.search(r'ACGT', enzyme):# check if the enzyme is a valid restriction enzyme
+    if not re.search(r'[^ACGT]+',sequence):# check if the sequence is a valid DNA sequence
+        if not re.search(r'[^ACGT]+', enzyme):# check if the enzyme is a valid restriction enzyme
             if re.search(enzyme, sequence):# check if the enzyme is present in the sequence
+                total_cut_sites = []# list to store the cut sites
                 for i in range(len(sequence)-len(enzyme)+1):# iterate through the sequence to find the enzyme
-                    total_cut_sites = []
-                    while sequence[i:i+len(enzyme)] == enzyme:
-                        cut_sites = [i, i+len(enzyme)-1]
+                    if sequence[i:i+len(enzyme)] == enzyme:
+                        cut_sites = [i]
                         total_cut_sites.append(cut_sites)
-                    return total_cut_sites
+                return total_cut_sites
             else:
                 print("No match")
                 return None
@@ -24,12 +24,6 @@ cut_sites = find_the_restriction_enzyme_cut_sites(sequence, enzyme)
 if cut_sites is not None:
     for cut_site in cut_sites:
         print(f"Cut site: {cut_site}") 
-# example input: sequence=ACGTACGTACGT, enzyme=ACGT
-# example output: Cut site: [0, 3], Cut site: [4, 7], Cut site: [8, 11]
-# example input: sequence=ACGTACGTACGT, enzyme=AGCT
-# example output: No match
-# example input: sequence=ACGTACGTACGT, enzyme=ACG
-# example output: enzyme not valid
-# example input: sequence=AGCTAGCTAGCT, enzyme=ACGT
-# example output: DNA sequence not valid
+# example input: sequence = "ACGTACGTACGT" enzyme = "ACGT"
+# example output: Cut site: [0] Cut site: [4] Cut site: [8]
 
