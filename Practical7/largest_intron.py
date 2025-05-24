b994fa4ec24d20d1be389393a4bb0de4ccdcca05
Yizheng <3275014244@qq.com>
2025-05-24 23:08:40 +0800

    update largest_intron

--- Practical7/largest_intron.py
+++ Practical7/largest_intron.py
@@ -1,10 +1,10 @@
 seq="ATGCAAGTGGTGTGTCTGTTCTGAGAGGGCCTAA" 
+max_length = 0
 for i in range(len(seq)): # iterate through the sequence
     if seq[i:i+2] == "GT": # check for the start codon
-       for j in range(i+2, len(seq)): 
+       for j in range(i+2, len(seq)): # iterate through the sequence
            if seq[j:j+2] == "AG": # check for the stop codon
                length = j - i + 2  # calculate the length of the intron
-               max_length = 0
                if length > max_length:# check if the length is greater than the current maximum length
                     max_length = length # update the maximum length
 print(max_length)
